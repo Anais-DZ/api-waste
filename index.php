16cc3991fb479af2e2a63ea0fc12d0774c9a1c42
Your Name <you@example.com>
2025-05-20 15:49:48 +0200

    header json

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 // Headers requis
 header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
+header("Content-Type: application/json");
 header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-AllowHeaders, Authorization, X-Requested-With");
